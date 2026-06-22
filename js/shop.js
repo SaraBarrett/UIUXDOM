5d76096b07540cdbc0e4dd057a3650f7ff48e803
SaraBarrett <sara.c.mon@gmail.com>
2026-06-22 16:10:51 +0100

    codigo comentado

--- js/shop.js
+++ js/shop.js
@@ -1,14 +1,21 @@
 let products = document.querySelectorAll("li");
 let myDiv = document.querySelector("div");
 
-console.log(products);
 function addToCart() {
+  //cria um paragrafo novo
   let myP = document.createElement("p");
+
+  //no paragrafo novo, adiciona o texto do elemento onde clicámos (this refere-se ao elemento de onde provém o clique definido abaixo no ciclo)
   myP.textContent = this.innerText;
 
+  //anexa o parágrafo à div do carrinho
   myDiv.appendChild(myP);
+
+  //muda o elemento clicado para a cor laranja
   this.style.backgroundColor = "orange";
 }
+
+//através da seleccção de todos os elementos lis, este cicli coloca todos eles à escuta, e sempre que eu clico num deles chama a função addToCart
 
 for (let element of products) {
   element.addEventListener("click", addToCart);
